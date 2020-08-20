34d0a312ccf1decb1d3410735c79e7da37ac9d4b
Eloilson Rocha <eloilson.ageedu@gmail.com>
2020-08-20 00:59:09 -0300

    Basic functionality completed

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -3,13 +3,13 @@ import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
+import api from '../../services/api';
 import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
-import api from '../../services/api';
 
 function Landing() {
   const { navigate } = useNavigation();
